45bc6b82b49300635ce53c509167df7cc4625aac
morgan.chabaud <mchabaud@enssat.fr>
2023-12-11 18:07:14 -0500

    feat: courbes d'apprentissage et analyse comportement

--- reseau_neurones.py
+++ reseau_neurones.py
@@ -1,4 +1,6 @@
-from sklearn.model_selection import GridSearchCV
+from matplotlib import pyplot as plt
+import numpy as np
+from sklearn.model_selection import GridSearchCV, learning_curve
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from modele import Modele
@@ -28,13 +30,23 @@ class Reseau_neurones(Modele):
         :return: le réseau de neurones avec les meilleures hyperparamètres
         """
         param_grid = {
-            'solver': ['lbfgs','sgd','adam'],
+            #'solver': ['lbfgs','sgd','adam'],
+            'solver': ['adam'],
             'hidden_layer_sizes': [(10,), (50,), (100,)],
-            'activation': ['relu', 'logistic', 'tanh'],
+            # 'activation': ['relu', 'logistic', 'tanh'],
+            'activation': ['tanh'],
             'alpha': [1e-3, 1e-4, 1e-5],
-            'max_iter': [500, 600, 700]
+            'max_iter': [500]
         }
         grid = GridSearchCV(estimator=self.model, param_grid=param_grid, cv=5, scoring='accuracy')
         grid.fit(X_train, y_train)
         print(grid.best_score_, " pour les hyperparamètres suivants : ", grid.best_params_)
         return grid.best_estimator_
+    
+    def plot_learning_curves(self, X_train, y_train, train_size):
+        N, train_score, val_score = learning_curve(self.model, X_train, y_train,train_sizes=train_size)
+        plt.plot(N,train_score.mean(axis=1),label="score d'entraîenemnt")
+        plt.plot(N,val_score.mean(axis=1),label="score de validation")
+        plt.xlabel("taille de l'ensemble d'entraînement")
+        plt.legend()
+
