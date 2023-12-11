31d4a9b948b324bbb37896f727c3bceb0829169a
morgan.chabaud <mchabaud@enssat.fr>
2023-12-10 23:42:06 -0500

    feat: complétion modèle svm + notebook

--- svm.py
+++ svm.py
@@ -1,44 +1,71 @@
+import numpy as np
 from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV, StratifiedShuffleSplit, train_test_split
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, LabelEncoder
 from sklearn import metrics
 from abc import ABC, abstractmethod
 import pandas as pd
 
-from projet_ift712.modele import Modele
-
-#######################
-# A RETIRER PLUS TARD #
-#######################
-# Importation des données
-df = pd.read_csv('heart.csv')  # Dataframe contenant les données
-features_names = df.columns
-features_nbr = features_names.shape[0]
-print(f"nombre de features dans le dataset : {features_nbr}")
-# Visualisation des données pour mieux les comprendre
-print(df.head())
-print(df.dtypes)
-le = LabelEncoder()
-
-data = df.copy(deep=True)
-
-data['Sex'] = le.fit_transform(data['Sex'])
-data['ChestPainType'] = le.fit_transform(data['ChestPainType'])
-data['RestingECG'] = le.fit_transform(data['RestingECG'])
-data['ExerciseAngina'] = le.fit_transform(data['ExerciseAngina'])
-data['ST_Slope'] = le.fit_transform(data['ST_Slope'])
+from modele import Modele
 
 
 class svm(Modele):
-    def __init__(self, data, features_names=0, features_nbr=0):
-        # Appel du constructeur de la classe parente
-        super().__init__(data, features_names, features_nbr, model=SVC(kernel='linear'))
+    def __init__(self, data, features_names=0, features_nbr=0, model=None):
+        Modele.__init__(
+            self=self,
+            data=data,
+            features_names=features_names,
+            features_nbr=features_nbr
+        )
+        match model:
+            case 'linear':
+                self.model = SVC(kernel='linear')
+            case 'rbf':
+                self.model = SVC(kernel='rbf')
+            case 'sigmoid':
+                self.model = SVC(kernel ='sigmoid')
+            case 'poly' :
+                self.model = SVC(kernel='poly')
+            case _ :
+                self.model = model
+    
+    def hyper_parameters_search(self, X_train, y_train):
+        """Recherche les meilleurs hyperparamètres pour la SVM dépendemment du type de noyau utilisé par 'grid search'.
+        Affiche les meilleurs hyperparamètres et retourne le modèle associé.
 
-    # Vous pouvez ajouter des méthodes spécifiques à la classe svm ici
+        :param X_train: Tableau (N, D) contenant les données d'entraînement
+        :param y_train: Tableau (N,1) contenant les label des données d'entraînement
 
-
-# Créez une instance de la classe svm
-svm_instance = svm(data, features_names, features_nbr)
-X_train, X_test, y_train, y_test = svm_instance.split_data(data)
-svm_instance.train(X_train, y_train)
-svm_instance.evaluate_model(X_test, y_test)
+        :return: la svm avec les meilleures hyperparamètres dépendemment du noyau utilisé
+        """
+        if self.model.kernel == 'rbf':
+            C_range = [1,10,100,1000]
+            gamma_range = [1,0.1,0.001,0.0001]
+            param_grid = dict(gamma=gamma_range, C=C_range)
+            cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
+            grid = GridSearchCV(self.model, param_grid=param_grid, cv=cv, scoring='accuracy')
+            grid.fit(X_train, y_train)
+            print(grid.best_score_, " pour les hyperparamètres suivants, noyau RBF : ", grid.best_params_)
+            return grid.best_estimator_
+        elif self.model.kernel == 'sigmoid':
+            coef0_range = [-5, -3, -1, -0.1, 0.0, 0.1, 1]
+            param_grid = dict(coef0=coef0_range)
+            cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
+            grid = GridSearchCV(self.model, param_grid=param_grid, cv=cv, scoring='accuracy')
+            grid.fit(X_train, y_train)
+            print(grid.best_score_, " pour les hyperparamètres suivants, noyau sigmoïde : ", grid.best_params_)
+            return grid.best_estimator_
+        elif self.model.kernel == 'poly':
+            coef0_range = [-5, -3, -1, -0.1, 0.0, 0.1, 1]
+            degree_range = [2, 3, 4, 5, 6, 7]
+            param_grid = dict(coef0=coef0_range, degree = degree_range)
+            cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
+            grid = GridSearchCV(self.model, param_grid=param_grid, cv=cv, scoring='accuracy')
+            grid.fit(X_train, y_train)
+            print(grid.best_score_, " pour les hyperparamètres suivants, noyau polynomial : ", grid.best_params_)
+            return grid.best_estimator_
+        print("Erreur recherche hyper-paramètres")
+        return self.model
+        
+        
+        
