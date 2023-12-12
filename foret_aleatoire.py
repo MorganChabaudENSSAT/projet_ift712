b3bfa48e2af0fcba40b0d3ccf9a0b5772f166e32
Caroline Wang <Caroline.Wang@telecomnancy.eu>
2023-12-11 19:14:21 -0500

    feat : ajout de graines aléatoires et de recherche aléatoire d'hyperparamètres (en plus de l'exhaustive)

--- foret_aleatoire.py
+++ foret_aleatoire.py
@@ -1,5 +1,6 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV, StratifiedShuffleSplit
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, StratifiedShuffleSplit
+import numpy as np
 
 from modele import Modele
 
@@ -7,9 +8,9 @@ class foret_aleatoire(Modele):
     def __init__(self, data, features_names=0, features_nbr=0, model=None):
         super().__init__(data=data, features_names=features_names, features_nbr=features_nbr)
         self.model = model
-        
-    def hyper_parameters_search(self, X_train, y_train):
-        """
+    
+    def random_hyper_parameters_search(self, X_train, y_train, seed):
+        """ Permet de rechercher les hyperparamètres optimaux à l'aide d'une recherche aléatoire avec de grands intervalles.
         Affiche les meilleurs hyperparamètres et retourne le modèle associé.
 
         :param X_train: Tableau (N, D) contenant les données d'entraînement
@@ -18,24 +19,51 @@ class foret_aleatoire(Modele):
         :return: la forêt aléatoire avec les meilleurs hyperparamètres
         """
         
-        # Définissez l'espace des hyperparamètres à explorer
+        # Les hyperparamètres
         param = {
-            'n_estimators': [10, 50, 100, 200],
-            'max_depth': [None, 10, 20, 30],
+            'n_estimators': [int(x) for x in np.linspace(10, 1000, 50)],
+            'max_depth': [None] + [int(x) for x in np.linspace(10, 110, 10)],
             'min_samples_split': [2, 5, 10],
             'min_samples_leaf': [1, 2, 4],
             'bootstrap': [True, False]
         }
+        
+        # Recherche aléatoire
+        cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=seed)
+        random_search = RandomizedSearchCV(self.model, param_distributions=param, cv=cv, scoring='accuracy', random_state=seed)
+        random_search.fit(X_train, y_train)
+        
+        # Les meilleurs hyperparamètres
+        best_estimator = random_search.best_estimator_
+        print(f'La meilleure précision trouvée est de {random_search.best_score_} pour les hyperparamètres suivants : {random_search.best_params_}')
+        return best_estimator
 
-        # Instanciez le modèle DecisionTreeClassifier
-        rf_model = RandomForestClassifier(criterion='log_loss')
+    
+    def hyper_parameters_search(self, X_train, y_train, seed):
+        """ Permet de rechercher les hyperparamètres optimaux à l'aide d'une recherche exhaustive, avec des invervalles pertinents, trouvés grâce à la recherche aléatoire.
+        Affiche les meilleurs hyperparamètres et retourne le modèle associé.
+
+        :param X_train: Tableau (N, D) contenant les données d'entraînement
+        :param y_train: Tableau (N,1) contenant les label des données d'entraînement
+        
+        :return: la forêt aléatoire avec les meilleurs hyperparamètres
+        """
+        
+        # Les hyperparamètres
+        param = {
+            'n_estimators': [int(x) for x in np.linspace(100, 200, 10)],
+            'max_depth': [None] + [int(x) for x in np.linspace(50, 60, 1)],
+            'min_samples_split': [9, 10, 11, 12],
+            'min_samples_leaf': [1, 2, 4],
+            'bootstrap': [True]
+        }
         
         # Recherche stratifiée et exhaustive
-        cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
+        cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=seed)
         grid = GridSearchCV(self.model, param_grid=param, cv=cv, scoring='accuracy')
         grid.fit(X_train, y_train)
 
-        # Utilisez le modèle avec les meilleurs paramètres
+        # Le modèle avec les meilleurs paramètres
         best_estimator = grid.best_estimator_
-        print(f'La meilleure accuracy trouvée est de {grid.best_score_} pour les hyperparamètres suivants : {grid.best_params_}')
+        print(f'La meilleure précision trouvée est de {grid.best_score_} pour les hyperparamètres suivants : {grid.best_params_}')
         return best_estimator
